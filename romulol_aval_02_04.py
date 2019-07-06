cf591717bfe8551c9ed6d3085849c47a17fe7a8d
romulolanferdini <49734990+romulolanferdini@users.noreply.github.com>
2019-07-05 22:48:51 -0300

    Exercício 04

---
+++ romulol_aval_02_04.py
@@ -0,0 +1,8 @@
+dia= int(input("Informe seu dia de nascimento:  "))
+mes = int(input("Informe seu mês de nascimento:  "))
+ano = int(input(" Informe seu ano de nascimento: "))
+print(f"Você nasceu no dia : {dia}" )
+print(f"mes : {mes} novenbro")
+print(f"no ano: {ano}")
+if mes == 06:
+    print("voce nasceu no inverno ")
