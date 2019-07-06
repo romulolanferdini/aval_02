1700184a44a894972dab8246b94b2ebeb2f8b299
romulolanferdini <49734990+romulolanferdini@users.noreply.github.com>
2019-07-05 22:40:54 -0300

    Exercício 2

---
+++ romuloL_aval_02_02.py
@@ -0,0 +1,9 @@
+dia1 = int(input("Digite o dia inicia: ")
+mes1 = int(input("Digite  mês inicia1: ")
+ano1 = int(input("Digite o ano inicial: ")
+
+dia2 = int(input("Digite o dia final: ")
+mes2 = int(input("Digite o mês final: ")
+ano2 = int(input("Digite o ano final: ")
+
+print((ano2 - ano1) * 365 + ((mes2 - mes1) * 30) + (dia2 - dia1))
